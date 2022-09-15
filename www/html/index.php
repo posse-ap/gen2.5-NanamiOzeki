0de7f435b5870b4af34e2fb264620a277389c8b1
NanamiOzeki <nanasmile7373@gmail.com>
2022-09-15 11:47:48 +0900

    ブラウザで「http://localhost/quiz.php?id=1」にアクセスすると、 　「東京の難読地名クイズ」 　設問は「高輪」「亀戸」が表示されましたか？

--- www/html/index.php
+++ www/html/index.php
@@ -2,16 +2,43 @@
 // phpinfo();
 require(dirname(__FILE__) . "/dbconnect.php");
 $id = filter_input(INPUT_GET, 'id');
-$stmt = $db->prepare("SELECT * FROM big_questions WHERE id=?");
+function big_questions($db, $id)
+{
+    $stmt = $db->prepare("SELECT * FROM big_questions WHERE id=?");
 
-// 一個め（はてなになっている位置を指定）,何を定義しているか
-$stmt->bindValue(1,$id);
-// (5) SQL実行
-$stmt->execute();
+    // 一個め（はてなになっている位置を指定）,何を定義しているか
+    $stmt->bindValue(1, $id);
+    // (5) SQL実行
+    $stmt->execute();
 
-// (6) 該当するデータを取得
-$data = $stmt->fetch();
-print_r($data["name"]);
+    // (6) 該当するデータを取得
+    global $data;
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    print_r($data["name"]);
+
+    $stmt = $db->prepare("SELECT * FROM big_questions WHERE id=?");
+}
+big_questions($db, $id);
+print_r($data);
+
+function questions($db, $id)
+{
+    $stmt = $db->prepare("SELECT * FROM questions WHERE big_question_id=?");
+
+    // 一個め（はてなになっている位置を指定）,何を定義しているか
+    $stmt->bindValue(1, $id);
+    // (5) SQL実行
+    $stmt->execute();
+
+    // (6) 該当するデータを取得
+    global $questions_data;
+    $questions_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r('<pre>');
+    print_r($questions_data);
+    print_r('</pre>');
+}
+
+questions($db, $id);
 
 // (7) データベースの接続解除
 $pdo = null;
@@ -30,7 +57,26 @@ $pdo = null;
 
 <body>
     <h1 class="title">ガチで東京の人しか解けない！#東京の難読地名クイズ</h1>
-    <script type="text/javascript" src="app.js"></script>
+    <?php for ($i=0; $i < count($questions_data); $i++) { ?>
+    <div class="wrapper" id="wrapper">
+        <div class="content">
+            <h2 class="question">.この地名はなんて読む？</h2>
+            <div class="img">
+                <img src="<?php echo "./imgs/" . $questions_data[$i]['image'] ?>" alt="">
+            </div>
+            <ul class="selections">
+                <li class="selection" id="selection"></li>
+                <li class="selection" id="selection"></li>
+                <li class="selection" id="selection"></li>
+            </ul>
+        </div>
+        <div class="correctBox" id="correctBox">
+            <b class="correctTitle" id="correctTitle">正解！</b>
+            <b class="uncorrectTitle" id="uncorrectTitle">不正解！</b>
+            <p>正解は「」です！</p>
+        </div>
+    </div>
+    <?php } ?>
 </body>
 
 </html>
